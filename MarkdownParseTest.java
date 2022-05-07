1014632193819f8b61ce05e2055c168bce51c41e
Luis D Pina <cs15lsp22ale@ieng6-202.ucsd.edu>
2022-05-06 18:52:42 -0700

    updated MDPtest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -10,10 +10,6 @@ import java.util.ArrayList;
 //javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
 //java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
 public class MarkdownParseTest {
-    @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
-    }
 
     @Test
     public void testGetLinks() throws IOException
